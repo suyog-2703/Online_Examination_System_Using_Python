f54794ea78f7cc8fe49688762043a298174ba922
suyog-2703 <107184622+suyog-2703@users.noreply.github.com>
2024-06-13 09:02:19 +0530

    Update settings.py

--- onlinexamination/onlinexam/settings.py
+++ onlinexamination/onlinexam/settings.py
@@ -82,10 +82,15 @@ WSGI_APPLICATION = 'onlinexam.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'test', 
+        'USER': 'root',  
+        'PASSWORD': '******',  # mention ur pass here
+        'HOST': 'localhost',  #
+        'PORT': '3306',  
     }
 }
+
 
 
 # Password validation
